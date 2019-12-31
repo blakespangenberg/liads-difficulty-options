b38e6aad5f3af5c6359563fb6c0184e83b0bc066
Blake Spangenberg <blake.spangenberg1@gmail.com>
2019-12-30 19:45:08 -0800

    Added entry point + harmony initialization

--- DifficultyOptions/DifficultyOptions/Class1.cs
+++ DifficultyOptions/DifficultyOptions/Class1.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+
+using UnityEngine;
+using Harmony;
+using UnityModManagerNet;
 
 namespace DifficultyOptions
 {
     static class Main
     {
+        public static UnityModManager.ModEntry mod;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            mod = modEntry;
+            // HarmonyInstance.DEBUG = true;
+
+            return true;
+        }
     }
 }
